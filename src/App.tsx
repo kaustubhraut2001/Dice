60c65553e5f0333e05ff7491d367cb2f5d778db6
Kaustubh <kaustubhr2001@gmail.com>
2024-02-14 00:14:42 +0530

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import one from "../assets/one.png"
+import two from "../assets/two.png"
+import three from "../assets/three.png"
+import four from "../assets/four.png"
+import five from "../assets/five.png"
+import six from "../assets/six.png"
+
 
 const App = () => {
   return (
